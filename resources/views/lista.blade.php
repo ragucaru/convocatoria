79d7554f415b776addbaba425d5b327ab6793b52
ragucaru <ragucaru80@gmail.com>
2020-04-01 22:25:17 -0600

    Avance Casa mejorando codigo

--- resources/views/lista.blade.php
+++ resources/views/lista.blade.php
@@ -1,39 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layout')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Lista de Especialistas</title>
-  </head>
-    <body>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <div class="container">
+    
 
-            <div class="row">
-                    <div class="col-md-6">
-                        <img src="images/salud.png" class="rounded float-left" alt="...">
-                        
-                    </div>
-                    <div class="col-md-6">
-                        <img src="images/chiapas.png" class="rounded float-right" alt="...">
-                        
-                    </div>
-            </div>
-         </div>
-         <div class="container">
+@section('title','Lista de Especialistas')
+
+@section('content')
+ 
+         <div class="container">         
             <div class="row justify-content-md-center">
                 <h5>Lista de Especialistas Bolsa de Trabajo</h5>
             </div>
         </div>
-        <div class="container">
-        
+        <div class="container">        
             <div class="row justify-content-center">
                 <div class="col-md-12">
                     <div class="card">
@@ -116,5 +95,4 @@
             </div>
         </div>
 
-    </body>
-</html>
+@endsection
